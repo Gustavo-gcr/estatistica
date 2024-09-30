343c87bf58725f8ae683cdb27b9e82dc4ddafba6
Davidsb04 <caisilvasb756@hotmail.com>
2024-09-30 16:56:08 -0300

    Tabela Contigencia

--- estatistica.py
+++ estatistica.py
@@ -193,11 +193,11 @@ if uploaded_file is not None:
             st.write(contingency_table)
 
             frequency_table = pd.crosstab(
-                df['company_size'], df['employment_type'])
+                df['employment_type'], df['company_size'])
             st.write(frequency_table)
 
             fig, ax = plt.subplots()
-            frequency_table.plot(kind='bar', ax=ax)
+            contingency_table.plot(kind='bar', ax=ax)
             st.pyplot(fig)
 
             st.write("Análise: O tamanho da empresa influencia significativamente a distribuição dos tipos de contratos de trabalho, com empresas maiores oferecendo mais contratos de tempo integral.")
